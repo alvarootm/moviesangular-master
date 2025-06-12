ede30567f6f97b0f5eebbb3e872598d84e488ce7
alvarootm <alvarotorresmartinez22@gmail.com>
2025-06-12 08:47:42 +0200

    terminar

---
+++ src/app/interfaces/pelicula.interface.ts
@@ -0,0 +1,14 @@
+
+export interface IPelicula {
+  id: number;
+  titulo: string;              
+  tituloOriginal: string;      
+  descripcion: string;        
+  fechaEstreno: string;        
+  idiomaOriginal: string;      
+  popularidad: number;         
+  puntuacionPromedio: number;  
+  cantidadVotos: number;      
+  posterUrl: string;           
+  genero: string[];            
+}
